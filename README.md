1ebe357f14b338eb212fb552cdc58c03a3fc682d
albanybuipe96 <mohammedarahman96@gmail.com>
2023-06-08 11:46:05 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UtilsLibDemo
+
+A description of this package.
---
+++ Tests/UtilsLibDemoTests/UtilsLibDemoTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import UtilsLibDemo
+
+final class UtilsLibDemoTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(UtilsLibDemo().text, "Hello, World!")
+    }
+}
